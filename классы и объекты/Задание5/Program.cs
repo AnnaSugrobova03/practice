014e17b5577f87a2477499cf8ccd8ff141750979
Anna Sugrobova <101697765+AnnaSugrobova03@users.noreply.github.com>
2022-06-20 10:03:43 +0300

    Add files via upload

---
+++ классы и объекты/Задание5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Задание5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
